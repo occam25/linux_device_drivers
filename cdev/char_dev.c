1b3a3f3a0a2e6ae37799c4e8aa28aca0d8d7f128
Javi Jap <occam25@yahoo.es>
2014-09-21 18:31:43 +0200

    read/write printk improved

--- cdev/char_dev.c
+++ cdev/char_dev.c
@@ -43,10 +43,10 @@ int mychar_close(struct inode *inode, struct file *file){
 static ssize_t mychar_read(struct file *file, char __user *ubuf, size_t ubuf_len, loff_t *pos){
 	int nbytes;
 	struct mychar_dev *mychar_devp = (struct mychar_dev *)file->private_data;
-	printk(KERN_INFO "reading mychar device\n");
 	if((ubuf_len + *pos) > KMEM_SIZE){
 		ubuf_len = KMEM_SIZE - *pos;
 	}
+	printk(KERN_INFO "reading %d byte(s) from mychar device\n", ubuf_len);
 	nbytes = ubuf_len - copy_to_user(ubuf, mychar_devp->kmem + *pos, ubuf_len);
 	*pos += nbytes;
 	*(ubuf + (int)*pos) = '\0';
@@ -58,10 +58,10 @@ static ssize_t mychar_read(struct file *file, char __user *ubuf, size_t ubuf_len
 static ssize_t mychar_write(struct file *file, const char __user *ubuf, size_t ubuf_len, loff_t *pos){
 	int nbytes;
 	struct mychar_dev *mychar_devp = (struct mychar_dev *)file->private_data;
-	printk(KERN_INFO "writing mychar device\n");
 	if((ubuf_len + *pos) > KMEM_SIZE)
 		ubuf_len = KMEM_SIZE - *pos;
 	
+	printk(KERN_INFO "writting %d byte(s) to mychar device\n", ubuf_len);
 	nbytes = ubuf_len - copy_from_user(mychar_devp->kmem + *pos, ubuf, ubuf_len);
 	*pos += nbytes;
 	memset(mychar_devp->kmem, '\0', KMEM_SIZE);
