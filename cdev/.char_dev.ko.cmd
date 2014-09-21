50092a996553e7033d7570ff97c58e596d95d2d4
Javier Casas <javi@LaBTop.(none)>
2014-09-21 16:58:03 +0200

    clean up files

--- cdev/.char_dev.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/javi/LDD/linux_device_drivers/cdev/char_dev.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.8.0-32-generic/scripts/module-common.lds --build-id  -o /home/javi/LDD/linux_device_drivers/cdev/char_dev.ko /home/javi/LDD/linux_device_drivers/cdev/char_dev.o /home/javi/LDD/linux_device_drivers/cdev/char_dev.mod.o
--- cdev/.tmp_versions/char_dev.mod
+++
@@ -1,2 +0,0 @@
-/home/javi/LDD/linux_device_drivers/cdev/char_dev.ko
-/home/javi/LDD/linux_device_drivers/cdev/char_dev.o
